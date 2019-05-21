ac045c3a3f435ddf4412b8009d03b5c2fea765a3
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-21 19:11:07 +1200

    Set up get last location task and moved camera to position result

--- app/src/main/java/com/example/assignment2part3/MapsActivity.java
+++ app/src/main/java/com/example/assignment2part3/MapsActivity.java
@@ -3,27 +3,32 @@ package com.example.assignment2part3;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private GoogleMap mMap;
     private Button btnGetLocation;
-
     //Vars
     private int requestLocation = 99;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private String TAG = "Map App";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,18 +50,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     ActivityCompat.requestPermissions(MapsActivity.this,new String[]{
                             Manifest.permission.ACCESS_FINE_LOCATION},requestLocation);
                 }
+                //ini Fused location client
+                mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+                try{
+                   final Task <Location> currentLocation =mFusedLocationProviderClient.getLastLocation();
+                    currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            LatLng currentLatLng = new LatLng((double)currentLocation.getResult().getLatitude(),
+                                    (double)currentLocation.getResult().getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,14f));
+                        }
+                    });
 
+                }catch(SecurityException e){
+                    Log.e(TAG,"Get Location: Security Exception: "+e.getMessage());
+                }
             }
         });
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng auckland = new LatLng(-38, 175);
+        LatLng auckland = new LatLng(-37, 175);
         float zoom = 11f;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(auckland,zoom));
     }
-
 }
